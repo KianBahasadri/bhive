7e776ec357d38d30474e600bc42633ccd371dbcf
Kian_Bahasadri <101868619+KianBahasadri@users.noreply.github.com>
2024-07-04 13:51:30 -0400

    fixed user priv bug

--- hub/hub_initialize.sh
+++ hub/hub_initialize.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-if [[ $(whoami) != 'root' ]]
-then
-  echo this script needs to be run as root
-  exit 1
-fi
+set -e
 
-if [[ ! -d keys ]]
+if [[ $(whoami) != 'root' || ! -d keys || $# != 1]]
 then
-  echo create keys directory and put SSL certificates inside
+  echo To run this script, please ensure you have:
+  echo 1. Run it directly inside the hub directory from the bhive github repo
+  echo 2. Run it as root
+  echo 3. Created a directory called keys with your ssl files
+  echo 4. Passed the name of the user you would like to use as the first argument
   exit 1
 fi
 
 echo
+user="$1"
+echo Using User: "$user"
+alias runuser="setpriv --reuid=$user"
+
+echo
 echo Setting buildpath
-mkdir build
+runuser mkdir build
 buildpath=$(realpath ./build)
 echo Buildpath Set: "$buildpath"
 
 echo
 echo Sym-Linking bhive_data.json
-ln -s bhive_data_empty.json bhive_data.json
+runuser ln -sf bhive_data_empty.json bhive_data.json
+
+echo 
+echo Updating Packages
+apt update
 
 echo
 echo Setting up fail2ban
-apt install fail2ban
+apt install -y fail2ban
 echo Using default fail2ban configs for now
 
 echo
 echo Installing python packages
-apt install python3-pip
-pip install -r requirements.txt
+apt install -y python3-pip
+runuser pip install -r requirements.txt
 
 echo
 echo Installing Nginx
 apt install nginx
 echo Building Nginx conf
-./rebuild_nginx_conf.py
+runuser ./rebuild_nginx_conf.py
 echo Sym-Linking nginx conf to buildpath
 ln -sf "$buildpath"/nginx_bhive.conf /etc/nginx/conf.d/nginx_bhive.conf 
 echo Sym-Linking SSL Certificates
@@ -58,11 +67,12 @@ Host *
   SendEnv LANG LC_*
   HashKnownHosts yes
 END
-ln -sf /etc/ssh_config "$buildpath"/config
+ln -sf /etc/ssh_config "$buildpath"/ssh_config
+chmod 444 "$buildpath"/ssh_config
 
 echo
 echo Starting add-server endpoint
-fastapi run add_server.py --port 8001
+runuser fastapi run add_server.py --port 8001
 
 echo
 echo Configuring UFW rules
